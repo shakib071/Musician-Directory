5e428d31b8a7356d21c72664413a3321fc3b024d
shakib071 <shakibhasan071@gmail.com>
2025-03-20 06:32:19 +0600

    added exit musician after clicking musician name in table

--- Musicians_Directory/musician/urls.py
+++ Musicians_Directory/musician/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('add_musician/',views.add_musician, name="add_musician")
+    path('add_musician/',views.add_musician, name="add_musician"),
+    path('edit_musician/<int:id>/',views.edit_musician, name="edit_musician"),
 ]
--- Musicians_Directory/musician/views.py
+++ Musicians_Directory/musician/views.py
@@ -26,5 +26,14 @@ def add_musician(request):
     return render(request,'add_musician.html',{'form' : form})
 
 
-# def edit_musician(request,musician_id):
+def edit_musician(request,id):
+    musician  = MusicianModel.objects.get(pk=id)
+    musician_form = MusicianForm(instance=musician)
+    if request.method == "POST":
+        musician_form = MusicianForm(request.POST,instance=musician)
+        if musician_form.is_valid():
+            musician_form.save()
+            return redirect('homepage')
+    
+    return render(request,'add_musician.html',{'form': musician_form})
 
