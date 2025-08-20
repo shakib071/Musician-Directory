523d8e1dd6a7fe3e4973ed86f1351979e0540343
shakib071 <shakibhasan071@gmail.com>
2025-08-20 06:59:24 +0600

    added home page

--- Musicians_Directory/Musicians_Directory/views.py
+++ Musicians_Directory/Musicians_Directory/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render,redirect
 from album.models import AlbumModel
 def home(request):
     album_list = AlbumModel.objects.all()
-
     return render(request,'home.html',{'album_list': album_list})
