ecab284a29eedf352ae669f7f93db9913cab8a22
shakib071 <shakibhasan071@gmail.com>
2025-08-20 07:32:51 +0600

    added class based views

--- Musicians_Directory/musician/urls.py
+++ Musicians_Directory/musician/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('add_musician/',views.add_musician, name="add_musician"),
-    path('edit_musician/<int:id>/',views.edit_musician, name="edit_musician"),
+    path('add_musician/',views.MusicianCreateView.as_view(), name="add_musician"),
+    path('edit_musician/<int:id>/',views.MusicianUpdateView.as_view(), name="edit_musician"),
 ]
--- Musicians_Directory/musician/views.py
+++ Musicians_Directory/musician/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import MusicianModel 
 from .forms import MusicianForm
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -13,27 +15,47 @@ from .forms import MusicianForm
 
 
 
-def add_musician(request):
+# def add_musician(request):
 
-    if request.method == "POST":
-        form = MusicianForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("homepage")
+#     if request.method == "POST":
+#         form = MusicianForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("homepage")
         
-    else:
-        form =  MusicianForm()
-    return render(request,'add_musician.html',{'form' : form})
+#     else:
+#         form =  MusicianForm()
+#     return render(request,'add_musician.html',{'form' : form})
 
 
-def edit_musician(request,id):
-    musician  = MusicianModel.objects.get(pk=id)
-    musician_form = MusicianForm(instance=musician)
-    if request.method == "POST":
-        musician_form = MusicianForm(request.POST,instance=musician)
-        if musician_form.is_valid():
-            musician_form.save()
-            return redirect('homepage')
+
+
+
+class MusicianCreateView(CreateView):
+    model = MusicianModel
+    form_class = MusicianForm
+    template_name = 'add_musician.html'
+    success_url = reverse_lazy('homepage')
+
+
+
+
+
+# def edit_musician(request,id):
+#     musician  = MusicianModel.objects.get(pk=id)
+#     musician_form = MusicianForm(instance=musician)
+#     if request.method == "POST":
+#         musician_form = MusicianForm(request.POST,instance=musician)
+#         if musician_form.is_valid():
+#             musician_form.save()
+#             return redirect('homepage')
     
-    return render(request,'add_musician.html',{'form': musician_form})
+#     return render(request,'add_musician.html',{'form': musician_form})
+
+
+class MusicianUpdateView(UpdateView):
+    model = MusicianModel
+    form_class = MusicianForm
+    template_name = 'add_musician.html'
+    success_url = reverse_lazy('homepage')
 
